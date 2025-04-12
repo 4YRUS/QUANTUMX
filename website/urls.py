ce326a40ef4678e765e5f47f49388bb3afd289ae
Bipin Sai Surya G <bss220904@gmail.com>
2025-04-12 07:38:44 +0530

    First Commit

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('crypto/',views.crypto,name='crypto'),
+    path('exchange/',views.exchange,name='exchange'),
+    path('getresponse/',views.get_response,name='response'),
+    path('mutual/',views.mutual,name='mutual'),
+    path('mutualfund/<str:symbol>',views.mutualfund,name='mutualfund'),
+    path('stock/<str:symbol>',views.stock,name='stock'),
+    
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyCz0zBOuiOAv6iBkjGNse05NROnuKAtLRY")
+def chat(prompt):
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+    response = model.generate_content(prompt)
+    return response.text
+
+
 
 
 def home(request):
 	return render(request,'home.html',{})
+
+def mutual(request):
+	return render(request,'mutual.html',{})
+
+def mutualfund(request,symbol):
+	return render(request,'mutualfund.html',{'symbol':symbol})
+
+
+def stock(request,symbol):
+	return render(request,'stock.html',{'symbol':symbol.upper()})
+
+def crypto(request):
+	return render(request,'crypto.html',{})
+
+def exchange(request):
+	return render(request,'exchange.html',{})
+
+def get_response(request):
+	if request.method=='POST':
+		data = json.loads(request.body)
+		try:
+			return JsonResponse({'message':chat(data['message'])})
+		except:
+			return JsonResponse({'message':'NOT WORKING'})
+
+	return JsonResponse({"message":"Wrong Protocol"})
+
 	
